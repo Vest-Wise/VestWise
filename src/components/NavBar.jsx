b943078e6b226f4d1b8f35aea3e43f730665bb5f
Tiffany <139176913+talph101@users.noreply.github.com>
2025-03-28 10:06:55 -0500

    NavBar at top right of page. Only half way through.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,20 +5,23 @@ function NavBar () {
 
 
 return (
-    <>        
-     <div className="d-flex vh-100">
-      <Navbar bg="dark" variant="dark" className="flex-column vh-100">
-        <Container className="flex-column">
-          <Navbar.Brand href="#home">VestWise</Navbar.Brand>
-          <Nav className="flex-column">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Holder</Nav.Link>
-            <Nav.Link href="#pricing">Holder</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-     </div>
-    </>
+  <>        
+   <div className="d-flex w-100">
+    <Navbar bg="dark" variant="dark" expand="lg" className="w-100 position-fixed top-0" style={{ zIndex: 1030, width: '100vw' }}>
+    <Container fluid>
+      <Navbar.Brand href="#home">VestWise</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Holder</Nav.Link>
+        <Nav.Link href="#pricing">Holder</Nav.Link>
+      </Nav>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
+   </div>
+  </>
 )
 }
 
