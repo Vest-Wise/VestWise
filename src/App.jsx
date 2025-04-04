2522e3ad378c228dd2b628a8be01316f7487187b
Tiffany <139176913+talph101@users.noreply.github.com>
2025-04-04 10:53:41 -0500

    included react router details in the App.jsx. (nt)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar';
+import CardComponent from './components/Card';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-
-      <div>
-        <NavBar />
-      </div>
-  
-  )
+// removed divs. I don't htink we need them
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/stocknews" element={<CardComponent />} />
+      </Routes>
+    </Router>
+  );
 }
+      
 
 export default App
